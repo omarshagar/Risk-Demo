0d3fc40e8dc15dcb53089eec072eca6236850e2c
omarshagar <omar.shagar@gmail.com>
2019-12-09 22:27:22 -0800

    cmd game

---
+++ RiskCmd/riskDemoCmd/player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace riskdemo
+{
+    class player
+    {
+        public int notusedtroops;
+        public int usedtroops;
+        public int cards;
+        public int id;
+       // public  string name;
+        public int contents;
+        public int  countries;
+        public player()
+        {
+            notusedtroops = 35;
+            usedtroops = 35;
+            cards = 0;
+            countries = 0;
+        }
+
+    }
+}
